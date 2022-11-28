6640e8d43fb564edf46502010c4873c8b69f53d4
Sandeep Verma <sandeep@verma.me>
2022-11-27 17:46:07 -0800

    add time measurement

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,46 @@
 use std::collections::HashMap;
 
-fn collatz_helper(orig: u64, num: u64, cache: &mut HashMap<u64, u32>) -> u32 {
+fn collatz(orig: u64, num: u64, cache: &mut HashMap<u64, u32>) -> u32 {
     if cache.contains_key(&num) {
         *(cache.get(&num).expect("not computed yet"))
     } else {
         let computed = 1 + match num % 2 {
-            1 => collatz_helper(orig, num * 3 + 1, cache),
-            _ => collatz_helper(orig, num / 2, cache),
+            1 => collatz(orig, num * 3 + 1, cache),
+            _ => collatz(orig, num / 2, cache),
         };
-        if num <= orig {
-            cache.insert(num, computed);
-        }
+        // if num <= orig {
+        cache.insert(num, computed);
+        //}
         computed
     }
 }
 
 fn collatz_length(n: u64, cache: &mut HashMap<u64, u32>) -> u32 {
-    collatz_helper(n, n, cache)
+    collatz(n, n, cache)
 }
 
 fn main() {
+    use std::time::Instant;
+    let now = Instant::now();
     let mut max = 0;
+    let mut max_num = 0;
     let mut cache = HashMap::from([(1u64, 1u32)]);
-    for n in 1..1000000 {
+    for n in 1..100000000 {
         let c = collatz_length(n, &mut cache);
         if c > max {
-            max = c
+            max = c;
+            max_num = n;
         }
 
         // println!("{} => {}", &n, &c);
     }
+    let elapsed = now.elapsed();
 
-    println!("Maximum: {}, Cache Size: {}", max, cache.len());
+    println!(
+        "Maximum: collatz({}) {}, Cache Size: {}. Elapsed: {:.2?}",
+        max_num,
+        max,
+        cache.len(),
+        elapsed
+    );
 }
