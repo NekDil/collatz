73f1a26963696845176d3309988ef3d721d4c4ca
Sandeep Verma <sandeep@axiom.local>
2023-05-03 23:42:01 -0700

    make collatz iterative, use vec cache
    Use iteration, speed-up 2x.
    For 100M, M1 Pro
    Recursive - 38sec, Iterative 19sec
    Use vec cache improves further 20x
    Same input with vec cache - 980ms

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
-use std::collections::HashMap;
+fn collatz(num: u64, v: &mut Vec<u32>) -> u32 {
+    let mut n = num as usize;
+    let mut computed = 0u32;
 
-fn collatz(orig: u64, num: u64, cache: &mut HashMap<u64, u32>) -> u32 {
-    if cache.contains_key(&num) {
-        *(cache.get(&num).expect("not computed yet"))
-    } else {
-        let computed = 1 + match num % 2 {
-            1 => collatz(orig, num * 3 + 1, cache),
-            _ => collatz(orig, num / 2, cache),
-        };
-        // if num <= orig {
-        cache.insert(num, computed);
-        //}
-        computed
+    while n != 1 {
+        match v.get(n) {
+            Some(c) => {
+                computed = computed + c;
+                break;
+            }
+            _ => {
+                computed += 1;
+                if n % 2 == 1 {
+                    n = n * 3 + 1;
+                } else {
+                    n = n / 2;
+                }
+            }
+        }
     }
-}
-
-fn collatz_length(n: u64, cache: &mut HashMap<u64, u32>) -> u32 {
-    collatz(n, n, cache)
+    v.push(computed);
+    *(v.get(num as usize).unwrap())
 }
 
 fn main() {
@@ -24,9 +27,9 @@ fn main() {
     let now = Instant::now();
     let mut max = 0;
     let mut max_num = 0;
-    let mut cache = HashMap::from([(1u64, 1u32)]);
-    for n in 1..100000000 {
-        let c = collatz_length(n, &mut cache);
+    let mut cache = vec![1u32];
+    for n in 1..=100_000_000 {
+        let c = collatz(n, &mut cache);
         if c > max {
             max = c;
             max_num = n;
