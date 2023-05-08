b62e1e0449b19da7503ce2d0187b43f75df20cf2
Sandeep Verma <sandeep@verma.me>
2023-05-07 23:40:55 -0700

    early exit for even numbers

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,43 @@
-fn collatz(num: u64, cache: &mut Vec<u32>) -> u32 {
-    let mut n = num as usize;
+#[inline]
+fn collatz(num: usize, cache: &mut Vec<u32>) -> u32 {
+    let mut n = num;
     let mut computed = 0u32;
 
-    while n != 1 {
-        match cache.get(n) {
-            Some(c) => {
-                computed = computed + c;
-                break;
-            }
-            _ => {
-                computed += 1;
-                if n % 2 == 1 {
-                    n = n * 3 + 1;
-                } else {
-                    n = n / 2;
+    if n % 2 == 0 {
+        computed = *(cache.get(n >> 1).unwrap()) + 1;
+        cache.push(computed);
+        computed
+    } else {
+        while n != 1 {
+            match cache.get(n) {
+                Some(c) => {
+                    computed = computed + c;
+                    break;
+                }
+                _ => {
+                    computed += 1;
+                    if n % 2 == 1 {
+                        n = n * 3 + 1;
+                    } else {
+                        n >>= 1;
+                    }
                 }
             }
         }
+        cache.push(computed);
+        computed
     }
-    cache.push(computed);
-    *(cache.get(num as usize).unwrap())
 }
 
 fn main() {
     use std::time::Instant;
+    let limit = 100_000_000;
     let now = Instant::now();
     let mut max = 0;
     let mut max_num = 0;
     let mut cache = vec![1u32];
-    for n in 1..=100_000_000 {
+
+    for n in 1..=limit {
         let c = collatz(n, &mut cache);
         if c > max {
             max = c;
